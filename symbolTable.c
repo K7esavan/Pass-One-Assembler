013605dafd17a1a16372a2cd37a83d75a3d16a70
Kesavan M <m.k7esavan@gmail.com>
2021-03-19 23:27:46 +0530

    added comments to symbolTable.c

--- symbolTable.c
+++ symbolTable.c
@@ -31,23 +31,31 @@ void insertToHashTable(
     /* Check whether the given name is valid */
     if (checkWhetherStringIsValid(nameOfSymbol)) {
 
+        /* Creting new Symbol */
         Symbol *newSymbol = (Symbol*) malloc (1 * sizeof(Symbol));
+
+        /* finding exact position to include it on hashtable */
         int position = hashFunction(nameOfSymbol[0]);
 
+        /* If that position is empty */
         if (myHashTable->chain[position] == NULL) {
-
             myHashTable->chain[position] = newSymbol;
+
         } else {
 
+            /* That position is not empty so traverse through that particular list */
             list lp = myHashTable->chain[position];
             while(lp != NULL) {
 
                 lp = lp->next;
             }
+
+            /* got the right place to insert, insert it */
             lp = newSymbol;
         }
 
     } else {
+        /* According to my professor this is important */
         error("Invalid Name for the Symbol in inserting to hash Table ");
     }
 }
